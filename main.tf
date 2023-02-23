3e20a98e4cc75aa820216ac393f7ed3de17d6257
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2023-02-23 00:18:42 -0500

    Adding vm

--- main.tf
+++ main.tf
@@ -16,5 +16,37 @@ terraform {
 }
 
 
-provider "google" {
+provider "google" {}
+
+
+
+resource "google_compute_instance" "default" {
+  name         = "jupyterlab-a11y-testing"
+  machine_type = "e2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = " ubuntu-2204-jammy-v20230214"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "curl -L https://tljh.jupyter.org/bootstrap.py | sudo python3 - --admin costrouc"
 }
